b87ccaa525f57df2b81eb9c993d769029251d8b7
rich <morenoricardo237@gmail.com>
2017-07-12 15:28:08 -0400

    Simple changes (not important)

--- app/decorators.py
+++ app/decorators.py
@@ -3,12 +3,12 @@ from flask import render_template, url_for, redirect, session
 from helpers import session_exists
 
 
-def check_session(f):
-    @wraps(f)
+def check_session(func):
+    @wraps(func)
     def new_function(*args, **kargs):
         if session_exists('username'):
             user = session['username']
             return redirect(url_for('user.profile', user=user))
         else:
-            return f(*args, **kargs)
+            return func(*args, **kargs)
     return new_function
